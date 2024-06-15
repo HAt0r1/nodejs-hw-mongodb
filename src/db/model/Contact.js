ae9965748218c6a35affb2b67a2c817bca319472
Hatori <ahgangadm@gmail.com>
2024-06-15 18:27:21 +0300

    Create contacts schema and model

---
+++ src/db/model/Contact.js
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      optional: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      default: 'personal',
+      enum: ['work', 'home', 'personal'],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ContactModel = model('contact', contactSchema);
