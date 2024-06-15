d3eaf7e335da70cdcec6e2823528ad92c6907ac4
Hatori <ahgangadm@gmail.com>
2024-06-15 18:26:16 +0300

    Add initialization function for MongoDB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import setupServer from './server.js';
+import initMongoConnection from './db/initMongoConnection.js';
 
 const boostTrap = async () => {
   setupServer();
+  await initMongoConnection();
 };
 
 boostTrap();
