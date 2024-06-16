f23809a206e8beaa1f22d95a22ca11f2064034f3
Hatori <ahgangadm@gmail.com>
2024-06-16 18:46:41 +0300

    Update server function

--- src/server.js
+++ src/server.js
@@ -10,26 +10,21 @@ const PORT = env('PORT');
 const setupServer = () => {
   const app = express();
 
+  app.use(cors());
+
   app.use(
     pino({
-      transport: {
-        target: 'pino-pretty',
-      },
+      transport: { target: 'pino-pretty' },
     }),
   );
 
-  app.use(cors());
-
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
-
-    if (contacts) {
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-    }
+    res.status(200).json({
+      status: res.statusCode,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
@@ -37,22 +32,20 @@ const setupServer = () => {
     const contact = await getContactById(contactId);
 
     if (!contact) {
-      res.status(404).json({
-        status: 404,
-        message: `Cant find contact with id ${contactId}`,
+      res.status(400).json({
+        message: 'Contact  not found',
       });
     }
+
     res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}`,
+      status: res.statusCode,
+      message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   });
 
   app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
+    res.status(404).json({ message: 'Not found' });
   });
 
   app.listen(Number(PORT), () => {
