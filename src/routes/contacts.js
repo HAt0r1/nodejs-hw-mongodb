d2d6abf4bf3a38986367d0d8ccedaaa3083a35e8
Hatori <ahgangadm@gmail.com>
2024-06-22 18:56:14 +0300

    Add controller to post route

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -3,13 +3,14 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   getAllContactsController,
   getContactByIdController,
+  postContactController,
 } from '../controllers/contacts.js';
 
 const router = express.Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 
-router.post('/contacts', ctrlWrapper());
+router.post('/contacts', ctrlWrapper(postContactController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
