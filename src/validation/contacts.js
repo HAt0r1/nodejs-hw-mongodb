8175e4876fed06becd9039ab8ae5b5ef5cd22165
Hatori <ahgangadm@gmail.com>
2024-06-27 18:03:48 +0300

    Create validation update/create schemas

---
+++ src/validation/contacts.js
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+
+export const createStudentSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean().required(),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('personal', 'work', 'home')
+    .required(),
+});
+
+export const updateStudentSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20).valid('personal', 'work', 'home'),
+});
