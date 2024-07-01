6f4bf9fe9e328a4cf98cb30eb796edf885594a29
Hatori <ahgangadm@gmail.com>
2024-07-01 13:11:09 +0300

    Create constans to incapsulate logic of function

---
+++ src/constans/sortedParams.js
@@ -0,0 +1,14 @@
+export const sortedParams = {
+  ASC: 'asc',
+  DESC: 'desc',
+};
+
+export const keyPairsParams = [
+  '_id',
+  'name',
+  'phoneNumber',
+  'contactType',
+  'isFavourite',
+  'createdAt',
+  'updatedAt',
+];
