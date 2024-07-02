31c7bc3f965982bc4c91813b33be26176947b3da
Hatori <ahgangadm@gmail.com>
2024-07-02 13:09:48 +0300

    Remove required fields from update function

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,12 +4,8 @@ export const createStudentSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20),
-  isFavourite: Joi.boolean().required(),
-  contactType: Joi.string()
-    .min(3)
-    .max(20)
-    .valid('personal', 'work', 'home')
-    .required(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20).valid('personal', 'work', 'home'),
 });
 
 export const updateStudentSchema = Joi.object({
