7abdbb42139f2e142a8190615c023a7f0d2b351a
Hatori <ahgangadm@gmail.com>
2024-07-15 19:20:34 +0300

    Add 2 routes for reset password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { registrationValidation, loginValidation } from '../validation/auth.js';
+import {
+  registrationValidation,
+  loginValidation,
+  resetEmailValidation,
+  resetPasswordValidation,
+} from '../validation/auth.js';
 import { validateBody } from '../utils/validationBody.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
@@ -7,6 +12,8 @@ import {
   registrationUserController,
   loginUserController,
   logoutUserController,
+  resetEmailController,
+  resetPasswordController,
 } from '../controllers/auth.js';
 
 const router = express.Router();
@@ -26,5 +33,17 @@ router.post(
 router.post('/refresh', ctrlWrapper(refreshSessionController));
 
 router.post('/logout', ctrlWrapper(logoutUserController));
+
+router.post(
+  '/send-reset-email',
+  validateBody(resetEmailValidation),
+  ctrlWrapper(resetEmailController),
+);
+
+router.post(
+  '/reset-pwd',
+  validateBody(resetPasswordValidation),
+  ctrlWrapper(resetPasswordController),
+);
 
 export default router;
