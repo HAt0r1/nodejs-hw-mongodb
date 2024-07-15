5ee681c29f34c29b2c0018181911984f9cb40191
Hatori <ahgangadm@gmail.com>
2024-07-15 19:20:17 +0300

    Add 2 controllers to reset password with email

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,11 @@
-import { registration, login, logout, refresh } from '../services/auth.js';
+import {
+  registration,
+  login,
+  logout,
+  refresh,
+  resetWithEmail,
+  resetPassword,
+} from '../services/auth.js';
 
 export const registrationUserController = async (req, res) => {
   const user = await registration(req.body);
@@ -63,5 +70,24 @@ export const refreshSessionController = async (req, res) => {
     data: {
       accessToken: session.accessToken,
     },
+  });
+};
+
+export const resetEmailController = async (req, res) => {
+  await resetWithEmail(req.body.email);
+  res.status(200).json({
+    status: 200,
+    message: 'Reset password email has been successfully sent.',
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+
+  res.status(200).json({
+    status: 200,
+    message: 'Password has been successfully reset.',
+    data: {},
   });
 };
