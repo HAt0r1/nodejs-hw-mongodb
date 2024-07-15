a41fc17c3d83dd969d3c50196215bb4bc3c46ef1
Hatori <ahgangadm@gmail.com>
2024-07-15 19:21:59 +0300

    Add validation for 2 new routes

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -2,11 +2,20 @@ import Joi from 'joi';
 
 export const registrationValidation = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().max(20).required(),
+  email: Joi.string().email().max(30).required(),
   password: Joi.string().min(6).max(20).required(),
 });
 
 export const loginValidation = Joi.object({
-  email: Joi.string().email().max(20).required(),
+  email: Joi.string().email().max(30).required(),
   password: Joi.string().min(6).max(20).required(),
+});
+
+export const resetEmailValidation = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+export const resetPasswordValidation = Joi.object({
+  token: Joi.string().required(),
+  password: Joi.string().required(),
 });
